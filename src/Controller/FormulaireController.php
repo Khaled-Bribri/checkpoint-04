d361bf41187b92342dd47e02f7ac97bf79b5fb2c
Khaled Bribri <94048267+Khaled-Bribri@users.noreply.github.com>
2023-03-06 23:47:48 +0100

    page contact

--- src/Controller/FormulaireController.php
+++ src/Controller/FormulaireController.php
@@ -5,26 +5,30 @@ namespace App\Controller;
 use App\Entity\Formulaire;
 use App\Form\FormulaireType;
 use App\Services\MailerService;
+use Exception;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\Dotenv\Dotenv;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+
 #[Route('/contact')]
 class FormulaireController extends AbstractController
 {
     #[Route('/', name: 'app_formulaire')]
     public function index(Request $request, MailerService $mailerService): Response
-    {   
+    {
         $form = new Formulaire();
         $form = $this->createForm(FormulaireType::class, $form);
         $form->handleRequest($request);
-        if($form->isSubmitted() and $form->isValid() )
-        {
+        if ($form->isSubmitted() and $form->isValid()) {
             $contactData = $form->getData();
-            $mailerService->sendEmail($contactData->getSujet(),$contactData->getMessage(),$contactData->getEmail());
-            $this->addFlash('success', 'Vore message a été envoyé');
-            return $this->redirectToRoute('app_home');
+            try {
+                $mailerService->sendEmail($contactData->getSujet(), $contactData->getMessage(), $contactData->getEmail());
+                $this->addFlash('success', 'Vore message a été envoyé');
+                return $this->redirectToRoute('app_home');
+            } catch (Exception $e) {
+                echo "Error: " . $e->getMessage();
+            }
         }
 
         return $this->render('formulaire/index.html.twig', [
